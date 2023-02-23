e8e7088b288dee7c1853f9528a36910761651029
Jorja Fleming <aset284@gmail.com>
2023-02-22 18:29:38 -0700

    Show 4 basic array methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,16 @@ cats = ["Spatz", "Hammi", "Mookie", "Leopold"]
 ages = [8,5,13,10]
 weights = [12.5, 10.2, 7.4, 14.7]
 is_female = [true, false, false, false]
+# this method will remove the last false value
+last_gender = is_female.pop
+puts last_gender
+
+# this method removes the first weights 
+weights.shift
+
+# this method adds to the beginning of the array
+ages.unshift 13
+
+cats.push "Marvin"
+
+
